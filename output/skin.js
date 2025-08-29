d4199221a9da5a5bcc53acb571266fb7da2ea2ec
MG TOUR 360 GRAUS <108066237+mgtour360graus@users.noreply.github.com>
2025-08-29 11:53:56 -0300

    Update skin.js

--- output/skin.js
+++ output/skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 7.1.11/21010
 // Filename: google.ggsk
-// Generated 2025-08-29T11:40:10
+// Generated 2025-08-29T11:46:18
 
 function pano2vrSkin(player,base) {
 	player.addVariable('opt_3d_preview', 2, true, { ignoreInState: 1  });
